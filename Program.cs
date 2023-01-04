f209ae837c43e06f83c7f82230f93e34b75bcee8
unknown <gerasimmaks@mail.ru>
2023-01-04 20:48:31 +0300

    добавлен метод ввода значения ячейки массива

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿
+string [] EnterArray (int size)   
+{
+string [] NewArray = new string [size];
+for (int i = 0; i < NewArray.Length; i++)
+{
+Console.WriteLine ("enter the cell value"); 
+NewArray [i] = Console.ReadLine();
+}
+Console.WriteLine (string.Join(",", NewArray));
+return NewArray;
+}
+
+
+const int SIZE = 7;
+
+string [] Arr = EnterArray (SIZE);

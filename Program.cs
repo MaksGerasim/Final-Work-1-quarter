01336ca37649835d13b495392a8d015f3ee10828
unknown <gerasimmaks@mail.ru>
2023-01-04 22:03:45 +0300

    отработана первая часть задачи (подсчет размера массива)

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿
-string [] EnterArray (int size)   
+string [] enterArray (int size)   
 {
 string [] NewArray = new string [size];
 for (int i = 0; i < NewArray.Length; i++)
@@ -7,11 +7,53 @@ for (int i = 0; i < NewArray.Length; i++)
 Console.WriteLine ("enter the cell value"); 
 NewArray [i] = Console.ReadLine();
 }
-Console.WriteLine (string.Join(",", NewArray));
+Console.WriteLine (string.Join(",  ", NewArray));
 return NewArray;
 }
 
 
-const int SIZE = 7;
+void filterValuesOfArray (string[] Array, int ValuesFilter)
+{
+    int Index = 0;
+    int Count = 0;
 
-string [] Arr = EnterArray (SIZE);
+    while (Index < Array.Length)
+    {
+        if (Array[Index].Length < ValuesFilter +1 && Array[Index].Length > 0)
+        {
+            Count++;
+            Index++;
+        }
+        else
+        {
+            Index++;
+        }
+        Console.WriteLine (Count);
+    }
+
+
+
+
+
+    //return Count;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+const int SIZE = 7;    // размер массива
+const int VALUESFILTER = 3; // ввод значений фильтра, максимальное количество искомых символов в ячейке
+
+string [] Arr = enterArray (SIZE);
+filterValuesOfArray(Arr, VALUESFILTER);
+
+
+
